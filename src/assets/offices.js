207ae9e08d7876a420ac6cfea66b93aade89bb72
Przemyslaw Glebocki <glebocki.przemyslaw@gmail.com>
2021-03-22 12:43:12 +0100

    Change offices ids and add a template office later to fill in all locations

--- src/assets/offices.js
+++ src/assets/offices.js
@@ -1,50 +1,39 @@
 export const offices = [
   {
-    id: 1,
+    country: 'Country',
+    city: 'City',
+    address: '',
+    lonLat: [0, 0],
+  },
+  // AMERICAS
+  // EUROPE
+  // ASIA
+  // CIS
+
+  // TEMP
+  {
+    id: 1001,
     city: "Rome",
     lonLat: [12.5, 41.9],
   },
   {
-    id: 2,
+    id: 1002,
     city: "London",
     lonLat: [-0.12755, 51.507222],
   },
   {
-    id: 3,
+    id: 1003,
     city: "Madrid",
     lonLat: [-3.683333, 40.4],
   },
   {
-    id: 4,
+    id: 1004,
     city: "Paris",
     lonLat: [2.353, 48.8566],
   },
   {
-    id: 5,
+    id: 1005,
     city: "Berlin",
     lonLat: [13.3884, 52.5169],
   },
 ];
-
-// export const offices = [
-//   {
-//     id: 1,
-//     city: 'Kraków',
-//     country: 'Poland'
-//   },
-//   {
-//     id: 2,
-//     city: 'Gdańsk',
-//     country: 'Poland'
-//   },
-//   {
-//     id: 3,
-//     city: 'Warszawa',
-//     country: 'Poland'
-//   },
-//   {
-//     id: 4,
-//     city: 'Wrocław',
-//     country: 'Poland'
-//   },
-// ];
